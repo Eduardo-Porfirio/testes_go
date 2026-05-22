eac9dfa81e8be0011b0097384c2ec73cc2c091c3
Eduardo-Porfirio <eduardo.porfirio@estudante.uffs.edu.br>
2026-05-22 10:43:52 -0300

    First menu make in GO :construction:

--- cliapp.go
+++ cliapp.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/manifoldco/promptui"
+import (
+	"errors"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
 
 var debug int = 1 // Debug 1 for true 0 to disable output
 
@@ -10,34 +15,121 @@ type network_params struct {
 	name string
 }
 
-func call_opt(name string) {
-	println(
-		"Bem vindo " + name + " ao gerenciador de rede!" + "\n" + "==========================================",
-	)
-	//println("[ 1 ] - Network/IP configuration" + "\n" + "[ 2 ] - DNS" + "\n" + "[ 3 ] - Connection Test" + "\n" + "[ 4 ] - Exit")
+var templates = &promptui.SelectTemplates{
+
+	Label: `{{ "⚡" | yellow }} {{ . | bold | cyan }}`,
+
+	Active: `{{ "▸" | green | bold }} {{ . | green | bold }}`,
+
+	Inactive: `  {{ . | white }}`,
+
+	Selected: `{{ "✔" | green | bold }} {{ "Você escolheu:" | white }} {{ . | cyan }}`,
 }
 
-func get_input() {
+const (
+	op_one   = "[ 1 ] - Network/IP configuration"
+	op_two   = "[ 2 ] - DNS"
+	op_three = "[ 3 ] - Connection Test"
+	op_four  = "[ 4 ] - Exit"
+)
 
+const (
+	submenu_one   = "[ 1 ] - Change IP"
+	submenu_two   = "[ 2 ] - Manage Routes"
+	submenu_three = "[ 3 ] - Return"
+)
+
+func call_opt() {
 	prompt := promptui.Prompt{
 		Label: "Digite seu nome",
-	}
-
-	select_opt := promptui.Select{
-		Label: "Choose an option",
-		Items: []string{"[ 1 ] - Network/IP configuration", "[ 2 ] - DNS", "[ 3 ] - Connection Test", "[ 4 ] - Exit"},
 	}
 
 	name, err := prompt.Run()
 
 	if err != nil {
-		println("Sorry an error has ocurred! %v\n", err)
+		println("An error has ocurred! %p", err)
+	}
+
+	println(
+		"Bem vindo " + name + " ao gerenciador de rede!" + "\n" + "==========================================",
+	)
+}
+
+func submenu_opt1(options []string) {
+
+	selectedOpt := promptui.Select{
+		Label:     "Network/IP Configuration",
+		Items:     options,
+		Templates: templates,
+	}
+
+	_, opt, err := selectedOpt.Run()
+
+	if err != nil {
+		if errors.Is(err, promptui.ErrInterrupt) {
+			println("\n The operation has been interrupt by user!")
+			os.Exit(0)
+		}
+		println("Sorry, an execption has ocurred: %p\n", err)
+	}
+	switch opt {
+	case submenu_one:
+		println("Not Implemented!")
+	case submenu_two:
+		println("Not Implemented!")
+	case submenu_three:
 		return
 	}
+}
+
+func get_input() {
+
+	main_menu := []string{
+		op_one,
+		op_two,
+		op_three,
+		op_four,
+	}
+
+	submenu := []string{
+		submenu_one,
+		submenu_two,
+		submenu_three,
+	}
+
+	select_opt := promptui.Select{
+		Label:     "Choose an option",
+		Items:     main_menu,
+		Templates: templates,
+	}
+
 	//Call menu options
-	call_opt(name)
 
-	_, opt, err := select_opt.Run()
+	for {
+		_, opt, err := select_opt.Run()
 
-	println("You selected: %q\n", opt)
+		if err != nil {
+			if errors.Is(
+				err, promptui.ErrInterrupt,
+			) {
+				println("\n The operation has been interrupt by user!")
+				os.Exit(0)
+			}
+			println("Sorry, an execption has ocurred: %p\n", err)
+		}
+
+		//switch to make a decision based on input user
+
+		switch opt {
+		case op_one:
+			submenu_opt1(submenu)
+		case op_two:
+			println("Not implemented!")
+		case op_three:
+			println("Not implemented!")
+		case op_four:
+			os.Exit(0)
+		}
+
+	}
 }
